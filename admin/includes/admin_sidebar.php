445654500ca92e5ea134a895c31048b90606073d
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-08-27 13:04:38 +0600

    Updated IUT logo

--- admin/includes/admin_sidebar.php
+++ admin/includes/admin_sidebar.php
@@ -1,96 +1,96 @@
 <ul class="navbar-nav bg-gradient-success sidebar sidebar-dark accordion" id="accordionSidebar">
 
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
-                <div class="sidebar-brand-icon">
-                    <i class="fas fa-lock"></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">Admin</div>
-            </a>
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon">
+            <img style="width: 35px; height: auto;" src="../iutlogo/IUTLogo.png"/>
+        </div>
+        <div class="sidebar-brand-text mx-3">Admin</div>
+    </a>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
 
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item active">
-                <a class="nav-link" href="index.php">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-            </li>
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+    <!-- Divider -->
+    <hr class="sidebar-divider">
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Schedule
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Schedule
+    </div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+            aria-expanded="true" aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Meeting Details</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="meetings.php?source=add_meeting">Add Meeting</a>
+                <a class="collapse-item" href="meetings.php">View Meetings</a>
             </div>
+        </div>
+    </li>
 
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Meeting Details</span>
-                </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="meetings.php?source=add_meeting">Add Meeting</a>
-                        <a class="collapse-item" href="meetings.php">View Meetings</a>
-                    </div>
-                </div>
-            </li>
+    <!-- Nav Item - Previous Meetings -->
+    <li class="nav-item">
+        <a class="nav-link" href="meetings.php?source=previous_meetings">
+            <i class="fas fa-fw fa-clock"></i>
+            <span>Previous Meetings</span></a>
+    </li>
 
-            <!-- Nav Item - Previous Meetings -->
-            <li class="nav-item">
-                <a class="nav-link" href="meetings.php?source=previous_meetings">
-                    <i class="fas fa-fw fa-clock"></i>
-                    <span>Previous Meetings</span></a>
-            </li>
+    <!-- Nav Item - Pending Requests -->
+    <li class="nav-item">
+        <a class="nav-link" href="meetings.php?source=pending_requests">
+            <i class="fas fa-fw fa-pen"></i>
+            <span>Pending Requests</span></a>
+    </li>
 
-            <!-- Nav Item - Pending Requests -->
-            <li class="nav-item">
-                <a class="nav-link" href="meetings.php?source=pending_requests">
-                    <i class="fas fa-fw fa-pen"></i>
-                    <span>Pending Requests</span></a>
-            </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider">
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Permission
+    </div>
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Permission
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+            aria-expanded="true" aria-controls="collapsePages">
+            <i class="fas fa-fw fa-users"></i>
+            <span>Faculty</span>
+        </a>
+        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="faculty.php?source=add_faculty">Add Faculty</a>
+                <a class="collapse-item" href="faculty.php">View Faculties</a>
             </div>
+        </div>
+    </li>
 
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-                    aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fas fa-fw fa-users"></i>
-                    <span>Faculty</span>
-                </a>
-                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="faculty.php?source=add_faculty">Add Faculty</a>
-                        <a class="collapse-item" href="faculty.php">View Faculties</a>
-                    </div>
-                </div>
-            </li>
+    <!-- Nav Item - Profile -->
+    <li class="nav-item">
+        <a class="nav-link" href="profile.php">
+            <i class="fas fa-fw fa-user"></i>
+            <span>Profile</span></a>
+    </li>
 
-            <!-- Nav Item - Profile -->
-            <li class="nav-item">
-                <a class="nav-link" href="profile.php">
-                    <i class="fas fa-fw fa-user"></i>
-                    <span>Profile</span></a>
-            </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
 
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
 
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
-
-        </ul>
+</ul>
